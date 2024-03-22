2f8766a3f629baa2a766e1c381d0d9cc7aca3762
Livington <lopezcruzlivingtonlenin@gmail.com>
2024-03-22 17:00:26 -0500

    Probando creando botones

--- src/helpers.py
+++ src/helpers.py
@@ -1,30 +1,36 @@
 import pygame
 
 class Helpers ():
-    def __init__(self,screen_width:int,screen_height:int, resized_sprite_width:int,resized_sprite_height:int, number_row_sprite_sheet :int, number_column_sprite_sheet:int):
+    def __init__(self,screen_width:int,screen_height:int,resized_button_width : int, resized_button_height : int, resized_sprite_width:int,resized_sprite_height:int, number_row_sprite_sheet :int, number_column_sprite_sheet:int, list_label_sprite : list):
 
         self.screen_width = screen_width
         self.screen_height = screen_height
 
         self.resized_sprite_width = resized_sprite_width
         self.resized_sprite_height = resized_sprite_height
+
+        self.resized_button_height = resized_button_height
+        self.resized_button_width = resized_button_width
+
         self.number_row_sprite_sheet = number_row_sprite_sheet
         self.number_column_sprite_sheet = number_column_sprite_sheet
 
+        self.list_label_sprite = list_label_sprite
 
     def load_background(self, background : str):
-        background_init = pygame.image.load(f"assets/images/{background}.png")
+        background_init = pygame.image.load(f"assets/images/backgrounds/{background}.png")
         background_init = pygame.transform.scale(background_init, (self.screen_width, self.screen_height))
         return background_init
     
-    def load_sprite_sheet(self, sprite_sheet: str) -> list:
+    def load_sprite_sheet(self, sprite_sheet: str) -> dict:
         sprite_sheet_init = pygame.image.load(f"assets/sprites/{sprite_sheet}.png")
         
         sheet_width, sheet_height = sprite_sheet_init.get_size()
         sprite_width = sheet_width // 4 
         sprite_height = sheet_height // 6
+        
+        list_sprites = []
 
-        sprites_all = []
         for y in range(self.number_row_sprite_sheet):
             sprites_row = []
             for x in range(self.number_column_sprite_sheet):
@@ -36,6 +42,25 @@ class Helpers ():
 
                 sprites_row.append(resized_sprite) #[0,1,2,4]
 
-            sprites_all.append(sprites_row) #[[A],[B]...]
+            list_sprites.append(sprites_row) #[[A],[B]...]
+
+        sprites_all = {}
+
+        for label, sprite_row in zip(self.list_label_sprite, list_sprites):
+            sprites_all[label] = sprite_row
 
         return sprites_all
+    
+    def load_images_button(self,button_image:str):
+        button_init = pygame.image.load(f"assets/images/buttons/{button_image}.png")
+        button_init = pygame.transform.scale(button_init, (self.resized_button_width, self.resized_button_height))
+        return button_init
+    
+    def draw_button(self, screen : pygame.display , button_image : pygame.image, x: int, y:int, action_button, msg : str ):
+        button_rect = button_image.get_rect(topleft=(x, y))
+        screen.blit(button_image, button_rect)
+        
+        mouse_pressed = pygame.mouse.get_pressed()
+
+        if button_rect.collidepoint(pygame.mouse.get_pos()) and mouse_pressed[0]:
+            action_button(msg)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 
 import pygame
 import sys
+import time
 
 from helpers import Helpers
 
@@ -19,12 +20,26 @@ current_sprite_index = 0
 animation_speed = 200  # 200 ms
 clock = pygame.time.Clock()
 
-helper = Helpers(screen_width=400,screen_height=400,resized_sprite_width=100,resized_sprite_height=100,number_row_sprite_sheet=6, number_column_sprite_sheet=4)
+list_label_sprite = ["normal","sleep","eat","play","bath","dead"]
+
+helper = Helpers(screen_width=400,screen_height=400,resized_button_width=70,resized_button_height=70,resized_sprite_width=100,resized_sprite_height=100,number_row_sprite_sheet=6, number_column_sprite_sheet=4,list_label_sprite=list_label_sprite)
 
 background = helper.load_background(background="house_virtual_pet128x128")
 sprite_all = helper.load_sprite_sheet(sprite_sheet="virtual_pet_sprite_sheet128x192")
-sprite_a = sprite_all[0]
 
+button_sleep = helper.load_images_button(button_image="button_sleep_virtual_pet32x32")
+button_eat = helper.load_images_button(button_image="button_eat_virtual_pet32x32")
+button_play = helper.load_images_button(button_image="button_play_virtual_pet32x32")
+button_bath = helper.load_images_button(button_image="button_bath_virtual_pet32x32")
+
+sprite_a = sprite_all["normal"]
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+#Temporal
+def on_btn_click(message):
+    print(message)
 
 running = True
 while running:
@@ -36,6 +51,14 @@ while running:
     screen.blit(background, (0, 0))
     current_sprite = sprite_a[current_sprite_index]
     screen.blit(current_sprite, sprite_position)
+
+    #draw btns
+
+    helper.draw_button(screen=screen,button_image=button_sleep,x=0,y=100, action_button=on_btn_click,msg = "dormir")
+    helper.draw_button(screen=screen,button_image=button_eat,x=0,y=170, action_button=on_btn_click, msg = "comer")
+    helper.draw_button(screen=screen,button_image=button_play,x=0,y=240, action_button=on_btn_click, msg = "jugar")
+    helper.draw_button(screen=screen,button_image=button_bath,x=0,y=310, action_button=on_btn_click, msg = "banarse")
+
 
     pygame.display.flip()
 
